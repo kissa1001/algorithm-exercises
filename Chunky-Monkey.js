9dd94d02300b01966cc8ec6f3a86b3e1c38a3096
kissa1001 <kissa101295@gmail.com>
2016-09-21 23:27:13 -0400

    added chunky monkey problem

---
+++ Chunky-Monkey.js
@@ -0,0 +1,14 @@
+//Chunky Monkey 
+//Input: ["a", "b", "c", "d"], 2
+//Output: [["a", "b"], ["c", "d"]]
+
+//Using .slice()
+function chunkArrayInGroups(arr, size) {
+  arr = arr.slice();
+  var arr2 = [];
+  for(var i = 0, len = arr.length; i < len; i+=size) {
+    arr2.push(arr.slice(0, size));
+    arr = arr.slice(size);
+  }
+  return arr2;
+}
